de19ab6d23b736f5ce1459445aa40b4f8241f6f3
Administrator <Administrator@WIN-EQ7SMK6U6HB>
2024-06-01 16:50:26 +0200

    Listar Solicitudes casi Implementado (Mostrar titulo Curso pendiente)

--- src/main/java/repositories/SolicitudRepository.java
+++ src/main/java/repositories/SolicitudRepository.java
@@ -18,7 +18,7 @@ public interface SolicitudRepository extends JpaRepository<Solicitud, Integer> {
 	@Query("select e from Solicitud e where e.estadoSolicitud = ?1")
 	Collection<Solicitud> findByEstadoSolicitud(EstadoSolicitud estadoSolicitud);
 
-	@Query("select s from Solicitud s where s.momentoSolicitud < ?1")
+	@Query("select s from Solicitud s where s.momentoSolicitud > ?1")
 	Collection<Solicitud> findAllActive(Date currentMoment);
 
 	@Query("select r.solicitud from Registro r where r.dueño.id = ?1")
